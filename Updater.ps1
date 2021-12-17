ef40b53ea594d1ef60954f02136df863d6bcc5d2
burnitup545 <96095116+burnitup545@users.noreply.github.com>
2021-12-17 15:05:39 -0800

    Check for admin

--- Updater.ps1
+++ Updater.ps1
@@ -1,1 +1,2 @@
-if((([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544")) { "I am admin" | Out-File -FilePath $env:USERPROFILE\Desktop\worked.txt} else { "not admin" | Out-File -FilePath $env:USERPROFILE\Desktop\failed.txt}
+#Requires -RunAsAdministrator
+"Success!!" | Out-File -FilePath $env:USERPROFILE\Desktop\worked.txt
